6ed1cf9e157e14873fe6384e6248885a53280174
Mauricio Jourdan <40311351+jourdanmauricio@users.noreply.github.com>
2022-09-08 15:20:28 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Modules
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+
+@NgModule({
+  declarations: [AppComponent, SkeletonComponent, FooterComponent, NavigationComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  providers: [
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy,
+    },
+  ],
+
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
